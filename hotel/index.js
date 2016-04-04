ef48ecc463f762355da8cc6e806589d0285b9750
nitsanll <nitsan.ll13@gmail.com>
2016-04-04 18:50:23 +0300

    using es6

--- hotel/index.js
+++ hotel/index.js
@@ -1,72 +1,69 @@
+'use strict';
+
 var events = require('events');
 var utils = require('util');
-utils.inherits(Hotel, events.EventEmitter);
 
-function Hotel(name, type, rank)
+module.exports = class Hotel extends events 
 {
-    events.EventEmitter.call(this);
-    this.hotelName = name;
-    this.hotelType = type;
-    this.hotelRank = rank;
-    this.printObj= [""];     
-    this.displayHotel = function() 
+    constructor(name, type, rank)
     {
-        var str1 = ("Hotel's name: " + this.hotelName);
-        var str2 = ("Hotel's type: " + this.hotelType);
-        var str3 = ("Hotel's rank: " + this.hotelRank);
-        var str4 = ("");
-        var strArr = [str1, str2, str3, str4];
-        console.log(str1);
-        console.log(str2);
-        console.log(str3);
-        console.log(str4);
-        for(var i=0; i<4; i++)
-       
-        {
-            this.printObj.push(strArr[i]);
-            this.printObj.push("\n");
-        }
-    };
-}
-
-Hotel.prototype.addStar = function()
-{
-    this.hotelRank+=1;
-    this.emit('starAdded');
-};
-
-Hotel.prototype.removeStar = function()
-{
-    this.hotelRank-=1;
-    this.emit('starRemoved');  
-};
-
-Hotel.prototype.checkZero = function()
-{
-     if(this.hotelRank<0) 
-    {
-        this.hotelRank = 0;
-        var str = "Hotel's rank is 0 stars. cannot remove a star!";
-        console.log(str);
-        this.printObj.push(str);
-        this.printObj.push("\n");
-
+        super();
+        this.hotelName = name;
+        this.hotelType = type;
+        this.hotelRank = rank;
+        this.printObj= [``];     
     }
-    else 
+    displayHotel()      //prints hotel's details
     {
-        var str2 = "star has been removed!";
-        console.log(str2);
-        this.printObj.push(str2); 
-        this.printObj.push("\n");
-    }  
-};
+        var str1 = (`Hotel's name: ${this.hotelName}`);
+        var str2 = (`Hotel's type: ${this.hotelType}`);
+        var str3 = (`Hotel's rank: ${this.hotelRank} stars`);
+        var str4 = (``);
+        var strArr = [str1, str2, str3, str4];
+        console.log(`${str1}`);
+        console.log(`${str2}`);
+        console.log(`${str3}`);
+        console.log(``);
+        for(var i=0; i<4; i++)  
+        {
+            this.printObj.push(`${strArr[i]}`);
+            this.printObj.push(`\n`);
+        }
+    }
+    addStar()       //add a star to hotel's rank
+    {
+        this.hotelRank+=1;
+        this.emit('starAdded');
+    }
+    removeStar()        //remove a star from hotel's rank
+    {
+        this.hotelRank-=1;
+        this.emit('starRemoved');  
+    }
+    checkZero()         //anounce a star has been removed, and checks if amount of stars is under zero 
+    {
+         if(this.hotelRank<0) 
+        {
+            this.hotelRank = 0;
+            var str = `Hotel's rank is 0 stars. cannot remove a star!`;
+            console.log(`${str}`);
+            this.printObj.push(str);
+            this.printObj.push(`\n`);
 
-Hotel.prototype.displayAdd = function()
-{
-    var str = "star has been added!";
-    console.log(str);
-    this.printObj.push(str);
-    this.printObj.push("\n");
-};
-
-module.exports = Hotel;
+        }
+        else 
+        {
+            var str2 = `star has been removed!`;
+            console.log(`${str2}`);
+            this.printObj.push(str2); 
+            this.printObj.push(`\n`);
+        }  
+    }
+    displayAdd()        //anounce a star has been added
+    {
+        var str = `star has been added!`;
+        console.log(`${str}`);
+        this.printObj.push(str);
+        this.printObj.push(`\n`);
+    }
+}
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+'use strict';
+
 var events = require('events');
-var utils = require('util');
 var hotel = require('./hotel');
 var http = require('http');
 
 http.createServer(function(req,res){
     res.writeHeader(200);
-    res.write("success\n\n");
-    var myHotel_1 = new hotel("sheraton", "spa", 2);
-    var myHotel_2 = new hotel("moria", "spa", 3);
+    res.write(`success\n\n`);
+    
+    //creating Hotel instances
+    var myHotel_1 = new hotel('Sheraton', 'spa', 2);
+    var myHotel_2 = new hotel('Moria', 'spa', 3);
 
+    //callback functions
     myHotel_1.on("starAdded", myHotel_1.displayAdd);
     myHotel_1.on("starAdded", myHotel_1.displayHotel);
     myHotel_1.on("starRemoved", myHotel_1.checkZero);
@@ -19,19 +23,19 @@ http.createServer(function(req,res){
     myHotel_2.on("starRemoved", myHotel_2.checkZero);
     myHotel_2.on("starRemoved", myHotel_2.displayHotel);
 
+    //functions demo
     myHotel_1.removeStar();
     myHotel_1.removeStar();
     myHotel_1.removeStar();
     myHotel_1.addStar();
     myHotel_1.addStar();
 
-    myHotel_2.removeStar();
     myHotel_2.removeStar();
     myHotel_2.removeStar();
     myHotel_2.addStar();
     myHotel_2.addStar();
     
-    res.end(myHotel_1.printObj + myHotel_2.printObj);
-}).listen(8080,'127.0.0.1');
+    res.end(`${myHotel_1.printObj} ${myHotel_2.printObj}`); //sends the messages to browser
+}).listen(8080,`127.0.0.1`);
 
-console.log('listening on port 8080');
+console.log(`listening on port 8080\n`);
